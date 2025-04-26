5aa89ded6f5ef10f0f059b3ac8e8ce70756b8879
DaniilShomin <d.sh.v@yandex.ru>
2025-04-26 17:10:07 +0300

    fix python version

--- build.sh
+++ build.sh
@@ -2,7 +2,5 @@
 # скачиваем uv и запускаем команду установки зависимостей
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
-source .venv/bin/activate
-export PATH="$HOME/.local/bin:$PATH"
 make install
 
