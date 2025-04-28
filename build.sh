9e162dd4061ad3fd2b118317fe0cadee5978d714
DaniilShomin <d.sh.v@yandex.ru>
2025-04-29 00:50:50 +0300

    add line connect database

--- build.sh
+++ build.sh
@@ -3,3 +3,5 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 make install && psql -a -d $DATABASE_URL -f database.sql
+
+psql -h dpg-d078e9idbo4c73amo16g-a.oregon-postgres.render.com -U mydb_4uqa_user mydb_4uqa
