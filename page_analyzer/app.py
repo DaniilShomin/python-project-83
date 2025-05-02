ba06e6db14b370c7e4648d61360f9991706c98bc
DaniilShomin <d.sh.v@yandex.ru>
2025-05-02 19:02:30 +0300

    reduction timeout to request

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -121,7 +121,7 @@ def urls_checks(id):
     url = repo.find(id)
     error = False
     try:                
-        req = requests.get(url['name'], timeout=5)
+        req = requests.get(url['name'], timeout=2)
         req.raise_for_status()        
     except Exception:
         error = True
